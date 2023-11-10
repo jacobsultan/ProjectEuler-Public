c0e03670800abc9cdd4ea8759d3ddeb665173c71
Jacob Sultan <jsultanabc@gmail.com>
2023-11-10 14:19:42 +0000

    clean

--- project euler 1 - 50/projecteuler2.py
+++ project euler 1 - 50/projecteuler2.py
@@ -1,16 +1,18 @@
 import time
 start_time = time.time()
+
 tot = 0
-import time
 holder = 0
 curr = 1
 prev = 1
-while(curr < 400000000):
+
+while(curr < 4 * 10 ** 6):
     holder = curr
     curr += prev
     prev = holder
     if curr % 2 == 0:
         tot += curr
+
 print(tot)
 
 print(time.time() - start_time)
