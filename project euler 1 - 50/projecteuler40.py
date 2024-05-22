78fd8a77ceaba0f74a20a4bb49d13fb1464dd18e
jacobsultan <jsultanabc@gmail.com>
2024-05-22 16:18:45 +0100

    higher numbers to private

--- project euler 1 - 50/projecteuler40.py
+++
@@ -1,6 +0,0 @@
-string = "0."
-for i in range(1,1000002):
-    string += str(i)
-a = int(string[1000000 + 1]) * int(string[100001])  * int(string[10001]) * int(string[1001])
-a *= int(string[101]) * int(string[11]) * int(string[2])
-print(a)
